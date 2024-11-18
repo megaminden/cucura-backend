19fb7f416e01e72a0080f1b6cdb06c65649d36b7
Denford Kambarami <9839235+dkambarami@users.noreply.github.com>
2024-11-18 14:32:40 +0000

    added notifications and messaging routes

--- src/routes/message_routes.rs
+++ src/routes/message_routes.rs
@@ -10,7 +10,18 @@ pub fn message_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/messages/send").route(web::post().to(send_message)))
         .service(web::resource("/messages/delete/{id}").route(web::delete().to(delete_message)))
         .service(web::resource("/messages").route(web::get().to(find_all_messages)))
-        .service(web::resource("/messages/{id}").route(web::get().to(find_message)))
+        .service(
+            web::resource("/messages/sender/{sender_id}")
+                .route(web::get().to(find_message_by_sender)),
+        )
+        .service(
+            web::resource("/messages/receiver/{receiver_id}")
+                .route(web::get().to(find_message_by_receiver)),
+        )
+        .service(
+            web::resource("/messages/chat/{sender_id}/{receiver_id}")
+                .route(web::get().to(find_messages_between_parties)),
+        )
         .service(web::resource("/notifications/create").route(web::post().to(create_notification)))
         .service(web::resource("/notifications/{user_id}").route(web::get().to(get_notifications)))
         .service(
@@ -43,10 +54,51 @@ async fn send_message(client: web::Data<Client>, message: web::Json<Message>) ->
     }
 }
 
-pub async fn find_message(client: web::Data<Client>, path: web::Path<String>) -> impl Responder {
+pub async fn find_messages_between_parties(
+    client: web::Data<Client>,
+    path: web::Path<(String, String)>,
+) -> impl Responder {
     let collection: Collection<Message> = client.database("cucura-ccdb").collection("messages");
-    let id = path.into_inner();
-    let filter = doc! { "_id": ObjectId::parse_str(&id).unwrap() };
+    let (sender_id, receiver_id) = path.into_inner();
+    let filter = doc! { "sender": { "$in": [&sender_id, &receiver_id] },"receiver": { "$in": [&sender_id, &receiver_id] }};
+
+    let mut cursor = collection.find(filter).await.unwrap();
+
+    let mut messages = Vec::new();
+    while let Some(result) = cursor.next().await {
+        match result {
+            Ok(message) => messages.push(message),
+            Err(e) => eprintln!("Error: {:?}", e),
+        }
+    }
+    HttpResponse::Ok().json(messages)
+}
+
+pub async fn find_message_by_sender(
+    client: web::Data<Client>,
+    path: web::Path<String>,
+) -> impl Responder {
+    let collection: Collection<Message> = client.database("cucura-ccdb").collection("messages");
+    let sender_id = path.into_inner();
+    let filter = doc! { "sender": sender_id};
+
+    match collection.find_one(filter).await {
+        Ok(Some(message)) => HttpResponse::Ok().json(message),
+        Ok(None) => HttpResponse::NotFound().json("Message not found"),
+        Err(e) => {
+            eprintln!("Failed to find document: {}", e);
+            HttpResponse::InternalServerError().json("Failed to find message")
+        }
+    }
+}
+
+pub async fn find_message_by_receiver(
+    client: web::Data<Client>,
+    path: web::Path<String>,
+) -> impl Responder {
+    let collection: Collection<Message> = client.database("cucura-ccdb").collection("messages");
+    let receiver_id = path.into_inner();
+    let filter = doc! { "receiver": receiver_id};
 
     match collection.find_one(filter).await {
         Ok(Some(message)) => HttpResponse::Ok().json(message),
@@ -60,8 +112,8 @@ pub async fn find_message(client: web::Data<Client>, path: web::Path<String>) ->
 
 pub async fn delete_message(client: web::Data<Client>, path: web::Path<String>) -> impl Responder {
     let collection: Collection<Message> = client.database("cucura-ccdb").collection("messages");
-    let id = path.into_inner();
-    let filter = doc! { "_id": ObjectId::parse_str(&id).unwrap() };
+    let sender_id = path.into_inner();
+    let filter = doc! { "sender": sender_id };
 
     let delete_result = collection.delete_one(filter).await;
 
