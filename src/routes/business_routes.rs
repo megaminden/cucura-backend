b72d1891afeacdced3404ad33b7fae7793ad4e37
Denford Kambarami <9839235+dkambarami@users.noreply.github.com>
2024-12-01 23:39:40 +0000

    BSON - LEARNT MORE -\n PAGINATION

--- src/routes/business_routes.rs
+++ src/routes/business_routes.rs
@@ -10,6 +10,13 @@ use mongodb::{
 use uuid::Uuid;
 
 use crate::models;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct PaginationParams {
+    pub page: Option<u32>,
+    pub limit: Option<u32>,
+}
 
 pub fn business_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/businesses/register").route(web::post().to(register_business)))
@@ -48,22 +55,70 @@ pub async fn register_business(
     }
 }
 
+pub async fn find_business(client: web::Data<Client>, path: web::Path<String>) -> impl Responder {
+    let collection: Collection<Business> = client.database("cucura-ccdb").collection("businesses");
+    let business_id = path.into_inner();
+    let business_id_result = Uuid::parse_str(&business_id);
+    let business_id_uuid: Uuid;
+    match business_id_result {
+        Ok(uuid) => business_id_uuid = uuid,
+        Err(_) => return HttpResponse::BadRequest().json("Invalid business ID format"),
+    };
+    let filter = doc! { "business_id": Bson::Binary(bson::Binary { subtype: bson::spec::BinarySubtype::UserDefined(0), bytes: business_id_uuid.as_bytes().to_vec() }) };
+    match collection.find_one(filter).await {
+        Ok(Some(business)) => HttpResponse::Ok().json(business),
+        Ok(None) => HttpResponse::NotFound().json("Business not found"),
+        Err(e) => {
+            eprintln!("Failed to find document: {}", e);
+            HttpResponse::InternalServerError().json("Failed to find business")
+        }
+    }
+}
+
 pub async fn update_business(
     client: web::Data<Client>,
     business: web::Json<Business>,
 ) -> impl Responder {
     let collection: Collection<Business> = client.database("cucura-ccdb").collection("businesses");
-    let new_business = business.into_inner();
+    let mut new_business = business.into_inner();
     //check if business already exists
-    let filter = doc! { "name": &new_business.name };
+    let business_id_result = Uuid::parse_str(&new_business.business_id.to_string());
+    let business_id_uuid: Uuid;
+    match business_id_result {
+        Ok(uuid) => business_id_uuid = uuid,
+        Err(_) => return HttpResponse::BadRequest().json("Invalid business ID format"),
+    };
+
+    let binary_business_id = Bson::Binary(bson::Binary {
+        subtype: bson::spec::BinarySubtype::UserDefined(0),
+        bytes: business_id_uuid.as_bytes().to_vec(),
+    });
+    let filter = doc! { "business_id": &binary_business_id};
     let business_exists = collection.find_one(filter.clone()).await.unwrap();
     match business_exists {
         Some(_) => (),
         None => return HttpResponse::Ok().json("Error 10001 : Business does not exist"),
     }
 
-    let filter = doc! { "business_id":  bson::to_document(&new_business.business_id).unwrap() };
-    let update_doc = doc! { "$set": bson::to_document(&new_business).unwrap() };
+    //let update_doc = doc! { "$set": bson::to_document(&new_business).unwrap() };
+    let update_doc = doc! {
+        "$set": {
+            "name": &new_business.name,
+            "description": &new_business.description,
+            "logo": &new_business.logo,
+            "pictures": &new_business.pictures,
+            "founder": &new_business.founder,
+            "industry": &new_business.industry,
+            "phone": &new_business.phone,
+            "address": &new_business.address,
+            "city": &new_business.city,
+            "region": &new_business.region,
+            "country": &new_business.country,
+            "website": &new_business.website,
+            "contact_email": &new_business.contact_email,
+            "updated_at": &new_business.updated_at,
+        }
+    };
     let update_result = collection.update_one(filter, update_doc).await;
 
     match update_result {
@@ -91,31 +146,23 @@ pub async fn delete_business(client: web::Data<Client>, path: web::Path<String>)
     }
 }
 
-pub async fn find_business(client: web::Data<Client>, path: web::Path<String>) -> impl Responder {
-    let collection: Collection<Business> = client.database("cucura-ccdb").collection("businesses");
-    let business_id = path.into_inner();
-    let business_id_result = Uuid::parse_str(&business_id);
-    let business_id_uuid: Uuid;
-    match business_id_result {
-        Ok(uuid) => business_id_uuid = uuid,
-        Err(_) => return HttpResponse::BadRequest().json("Invalid business ID format"),
-    };
-    let filter = doc! { "business_id": Bson::Binary(bson::Binary { subtype: bson::spec::BinarySubtype::UserDefined(0), bytes: business_id_uuid.as_bytes().to_vec() }) };
-    match collection.find_one(filter).await {
-        Ok(Some(business)) => HttpResponse::Ok().json(business),
-        Ok(None) => HttpResponse::NotFound().json("Business not found"),
-        Err(e) => {
-            eprintln!("Failed to find document: {}", e);
-            HttpResponse::InternalServerError().json("Failed to find business")
-        }
-    }
-}
-
-pub async fn find_all_businesses(client: web::Data<Client>) -> impl Responder {
+pub async fn find_all_businesses(
+    client: web::Data<Client>,
+    query: web::Query<PaginationParams>,
+) -> impl Responder {
     let collection: Collection<Business> = client.database("cucura-ccdb").collection("businesses");
     let filter = doc! {};
 
-    let mut cursor = collection.find(filter).await.unwrap();
+    let page = query.page.unwrap_or(1);
+    let limit = query.limit.unwrap_or(20);
+    let skip = (page - 1) * limit;
+
+    let mut cursor = collection
+        .find(filter)
+        .await
+        .unwrap()
+        .skip(skip as usize)
+        .take(limit as usize);
 
     let mut businesses = Vec::new();
     while let Some(result) = cursor.next().await {
